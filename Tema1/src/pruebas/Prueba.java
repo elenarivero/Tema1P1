3fcd3157b605729872ad9e1a317a50953f0a75a7
elenarivero <145675391+elenarivero@users.noreply.github.com>
2024-09-24 14:18:18 +0200

    Ejercicio 9 corregido

--- Tema1/src/pruebas/Prueba.java
+++ Tema1/src/pruebas/Prueba.java
@@ -5,25 +5,11 @@ import java.util.*;
 public class Prueba {
 	
 	public static void main(String[] args) {		
-		// Variable que almacena la edad
-		int edad;
+		int num1 = 8;
+		num1 %= 2;
 		
-		// Variable para almacenar el nombre
-		String name;
-		
-		Scanner lectura = new Scanner(System.in);
-		
-		System.out.println("Introduzca su edad");
-		edad = lectura.nextInt();
-		lectura.nextLine();
-		
-		System.out.println(edad);
-		
-		System.out.println("Dime tu nombre");
-		
-		name = lectura.nextLine();
-		
-		System.out.println("Tu nombre es " + name);
+		System.out.println(num1);
+//		System.out.println(num2);
 	}
 
 }
